30f98bd1000daecd6489a33d9357c1b389b5e4bb
noelfoka <nomgnenoel@gmail.com>
2025-02-18 19:54:52 +0100

    debut de la landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <div>
-      <button className="btn btn-primary">Primary</button>
+      <div className="flex items-center justify-center flex-col py-10 w-full">
+        <div>
+          <div className="flex flex-col">
+            <h1 className="text-4xl md:text-5xl font-bold text-center">
+              Prenez le contrôle <br />de vos finances
+            </h1>
+
+            <p className="py-6 text-base-800 text-center">Suivez vos budgets et dépenses <br /> en toute simplicité avec notre application.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
