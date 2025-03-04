67b1af55c105ad8093ca859df37eae38a97dcc9d
noelfoka <nomgnenoel@gmail.com>
2025-03-04 06:03:12 +0100

    écupérer l'utilisateur dans la base de données

--- app/actions.ts
+++ app/actions.ts
@@ -1,12 +1,20 @@
 "use server"
 
+import prisma from "@/lib/prisma"
+
 // Vérifier si un utilisateur existe dans la base de données sinon l'ajouter
 
 export const checkAndAddUser = async (email: string | undefined) => {
   if(!email) return
 
   try {
-    
+    // Récupérer l'utilisateur dans la base de données
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    })
+      
   } catch (error) {
     console.error("Erreur lors de la vérification et ajout de l'utilisateur", error)
   }
