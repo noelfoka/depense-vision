daf88b750f69b7ae43196d3d49863dbe6fc9dca7
noelfoka <nomgnenoel@gmail.com>
2025-03-06 05:04:40 +0100

    Budget ajouté avec succès

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -3,15 +3,68 @@
 import { useUser } from "@clerk/nextjs";
 import Wrapper from "../components/Wrapper";
 import { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
+import { addBudget } from "../actions";
+import Notification from "../components/Notification";
 
 const Page = () => {
   const { user } = useUser();
   const [budgetName, setBudgetName] = useState<string>("");
-  const [budgetAmount, setBudgetAmount] = useState<string>(0);
+  const [budgetAmount, setBudgetAmount] = useState<string>("0");
+  const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
   const [selectedEmoji, setSelectedEmoji] = useState<string>("");
+  const [notification, setNotification] = useState<string>("");
+
+  const closeNotification = () => {
+    setNotification("");
+  }
+
+  const handleEmojiSelect = (emojiObject: {emoji: string}) => {
+    setSelectedEmoji(emojiObject.emoji);
+    setShowEmojiPicker(false);
+  }
+
+  const handleAddBudget = async () => {
+    try {
+      const amount = parseFloat(budgetAmount);
+
+      // Vérifier si le montant est positif ou pas
+      if (isNaN(amount) || amount <= 0) {
+        throw new Error("Le montant doit être un nombre positif");
+      }
+
+      await addBudget(
+        user?.primaryEmailAddress?.emailAddress as string,
+        budgetName,
+        amount,
+        selectedEmoji
+      )
+
+      const modal = document.getElementById("my_modal_3") as HTMLDialogElement;
+      if (modal) {
+        modal.close();
+      }
+
+      setNotification("Budget ajouté avec succès");
+
+      setBudgetName("");
+      setBudgetAmount("0");
+      setSelectedEmoji("");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <Wrapper>
+
+      {notification && (
+        <Notification
+          message={notification}
+          onClose={closeNotification}
+        />
+      )}
+
       <button
         className="btn"
         onClick={() =>
@@ -52,7 +105,16 @@ const Page = () => {
               className="input input-bordered mb-3"
             />
 
-            <button className="btn">Ajouter Budget</button>
+            <button className="btn btn-outline mb-3" onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
+              {selectedEmoji || "Selectioner un emoji"}
+            </button>
+
+            {showEmojiPicker && (
+              <div className="flex justify-center items-center my-4">
+                <EmojiPicker onEmojiClick={handleEmojiSelect} />
+              </div>
+            )}
+            <button onClick={handleAddBudget} className="btn">Ajouter Budget</button>
           </div>
         </div>
       </dialog>
